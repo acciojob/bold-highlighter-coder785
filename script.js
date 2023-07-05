0fe0864f77d188c41cb07d89bd4168a77830b10d
coder785 <116183840+coder785@users.noreply.github.com>
2023-07-05 22:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,5 +13,9 @@ function highlight() {
 function return_normal() {
     //Write your code here
 
-    
+    let stro=document.getElementsByTagName("strong");
+  let len=stro.length;
+  for(let i=0;i<len;i++){
+   stro[i].style.color="black";
+  }
 }
